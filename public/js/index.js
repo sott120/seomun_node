917f0b4977a150fc46869104eaba1e3b9a832049
sott120 <psy_sott120@naver.com>
2022-07-01 16:48:39 +0900

    메인페이지 js추가중

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+function fadeOut(el){
+    el.style.opacity = 1;
+  
+    (function fade() {
+      if ((el.style.opacity -= .1) < 0) {
+        el.style.display = "none";
+      } else {
+        requestAnimationFrame(fade);
+      }
+    })();
+  };
+  
+  function fadeIn(el, display){
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+  
+    (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += .1) > 1)) {
+        el.style.opacity = val;
+        requestAnimationFrame(fade);
+      }
+    })();
+  };
+
+
+let count=1;
+let imgchange=document.querySelector('#mainBnr')
+// setInterval(() => console.log(new Date()), 2000);
+setInterval(function mainBnr(){
+
+    imgchange.style.backgroundImage= "url(../img/index/mainBnr_img"+ count +".jpg)"
+    count++;
+    if(count>=3){count=1}
+},3000)
