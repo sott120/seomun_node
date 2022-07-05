5902c27ce8d68c90f1b3863c81a69034190f215c
sott120 <psy_sott120@naver.com>
2022-07-05 14:44:34 +0900

    카카오로그인 테스트

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,58 @@
-function fadeOut(el){
-    el.style.opacity = 1;
-  
-    (function fade() {
-      if ((el.style.opacity -= .1) < 0) {
-        el.style.display = "none";
-      } else {
-        requestAnimationFrame(fade);
-      }
-    })();
-  };
-  
-  function fadeIn(el, display){
-    el.style.opacity = 0;
-    el.style.display = display || "block";
-  
-    (function fade() {
-      var val = parseFloat(el.style.opacity);
-      if (!((val += .1) > 1)) {
-        el.style.opacity = val;
-        requestAnimationFrame(fade);
-      }
-    })();
-  };
+Kakao.init('df117fdcfa786c5e28a5629fb8c82776');
+Kakao.isInitialized();
+
+function kakaoLogin(){
+  Kakao.Auth.login({
+    success: function(response){
+      Kakao.API.request({
+        url:'v2/user/me',
+        success:function(response){
+          console.log(response);
+          // document.getElementById('user').innerText = 
+          // response.kakao_account.profile.nickname; //로그인 성공하면 유저이름 넣어주기
+        }
+      })
+    }
+  })
+};
+
+function kakaoLogout(){
+
+};
 
 
-let count=1;
-let imgchange=document.querySelector('#mainBnr')
-// setInterval(() => console.log(new Date()), 2000);
-setInterval(function mainBnr(){
+// function fadeOut(el){
+//     el.style.opacity = 1;
+  
+//     (function fade() {
+//       if ((el.style.opacity -= .1) < 0) {
+//         el.style.display = "none";
+//       } else {
+//         requestAnimationFrame(fade);
+//       }
+//     })();
+//   };
+  
+//   function fadeIn(el, display){
+//     el.style.opacity = 0;
+//     el.style.display = display || "block";
+  
+//     (function fade() {
+//       var val = parseFloat(el.style.opacity);
+//       if (!((val += .1) > 1)) {
+//         el.style.opacity = val;
+//         requestAnimationFrame(fade);
+//       }
+//     })();
+//   };
 
-    imgchange.style.backgroundImage= "url(../img/index/mainBnr_img"+ count +".jpg)"
-    count++;
-    if(count>=3){count=1}
-},3000)
+
+// let count=1;
+// let imgchange=document.querySelector('#mainBnr')
+// // setInterval(() => console.log(new Date()), 2000);
+// setInterval(function mainBnr(){
+
+//     imgchange.style.backgroundImage= "url(../img/index/mainBnr_img"+ count +".jpg)"
+//     count++;
+//     if(count>=3){count=1}
+// },3000)
