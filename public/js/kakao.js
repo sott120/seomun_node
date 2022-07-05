9d2c40d9910ad6dba5305e1e6f52f91aa446ac30
sott120 <psy_sott120@naver.com>
2022-07-05 17:44:07 +0900

    카카오 테스트

--- public/js/kakao.js
+++ public/js/kakao.js
@@ -3,8 +3,6 @@ Kakao.isInitialized();
 document.getElementById('logout').style.display = 'none';
 document.getElementById('userLi').style.display = 'none';
 
-console.log(Kakao.Auth);
-
 function kakaoLogin(){
   Kakao.Auth.login({
     success: function(response){
@@ -18,7 +16,6 @@ function kakaoLogin(){
           document.getElementById('userLi').style.display = 'block';
           document.getElementById('logout').style.display = 'block';
           alert(response.kakao_account.profile.nickname + '님 로그인 되었습니다.');
-          console.log(Kakao.Auth);
         }
       })
     }
