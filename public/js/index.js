7778b933bdb0f69dec1d2d24f50d9df2a6128bf9
sott120 <psy_sott120@naver.com>
2022-07-05 15:33:29 +0900

    카카오 로그인 테스트

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 Kakao.init('df117fdcfa786c5e28a5629fb8c82776');
 Kakao.isInitialized();
+document.getElementById('logout').style.display = 'none';
+document.getElementById('userLi').style.display = 'none';
 
 function kakaoLogin(){
   Kakao.Auth.login({
@@ -11,6 +13,8 @@ function kakaoLogin(){
           document.getElementById('user').innerText = 
           response.kakao_account.profile.nickname; //로그인 성공하면 유저이름 넣어주기
           document.getElementById('login').style.display = 'none';
+          document.getElementById('userLi').style.display = 'block';
+          document.getElementById('logout').style.display = 'block';
           alert(response.kakao_account.profile.nickname + '님 로그인 되었습니다.');
         }
       })
@@ -19,7 +23,21 @@ function kakaoLogin(){
 };
 
 function kakaoLogout(){
-
+  if (!Kakao.Auth.getAccessToken()) {
+    Kakao.API.request({
+      url:'/v1/user/unlink',
+      success:function(response){
+        // console.log(response);
+        document.getElementById('user').innerText = 
+        response.kakao_account.profile.nickname; //로그인 성공하면 유저이름 넣어주기
+        document.getElementById('login').style.display = 'block';
+        document.getElementById('logout').style.display = 'none';
+        document.getElementById('userLi').style.display = 'none';
+        alert(response.kakao_account.profile.nickname + '님 로그아웃 되었습니다.');
+      }
+    })
+    Kakao.Auth.setAccessToken(undefined);
+  }
 };
 
 
